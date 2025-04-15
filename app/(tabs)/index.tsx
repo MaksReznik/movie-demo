11a8dd1b7bfc3a3a530c160f0c78d45b372ee40e
Max Reznik <maks.reznik.02@gmail.com>
2025-04-15 22:14:21 +0300

    Implement tab navigation

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,13 @@
 import { Text, View } from 'react-native';
-import './globals.css';
+import '../globals.css';
+import { Link } from 'expo-router';
 export default function Index() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
       <Text text-primary className="text-5xl">
         Hi.
       </Text>
+      <Link href="/onboarding">Onboarding</Link>
     </View>
   );
 }
