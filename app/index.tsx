4b445c55cb7564805b0969ac3bddc85fc7679c0d
Max Reznik <maks.reznik.02@gmail.com>
2025-04-15 12:57:49 +0300

    Init expo project

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native';
+import './globals.css';
+export default function Index() {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text text-primary className="text-5xl">
+        Hi.
+      </Text>
+    </View>
+  );
+}
